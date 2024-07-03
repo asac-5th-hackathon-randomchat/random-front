0ae3f8f7c36badc0209d28db1940c3a3df28b9db
IforCU <tmdgus0844@gmail.com>
2024-07-03 22:55:29 +0900

    Feat: tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import React, { useState } from 'react'
 import StompComponent from './StompComponent'
 
@@ -7,6 +6,7 @@ function App() {
   const [roomInputValue, setRoomInputValue] = useState('')
   const [username, setUsername] = useState('')
   const [nameInputValue, setNameInputValue] = useState('')
+  const [isConnected, setIsConnected] = useState(false)
 
   const handleRoomInputChange = event => {
     setRoomInputValue(event.target.value)
@@ -16,59 +16,64 @@ function App() {
     setNameInputValue(event.target.value)
   }
 
-  const handlerConnectChannel = () => {
+  const handleConnectChannel = () => {
     setChatRoomId(roomInputValue)
     setUsername(nameInputValue)
+    setIsConnected(true)
     console.log(`Connecting to chat room: ${roomInputValue} as ${nameInputValue}`)
   }
 
-  const handlerDisconnectChannel = () => {
+  const handleDisconnectChannel = () => {
     setChatRoomId('')
     setRoomInputValue('')
     setUsername('')
     setNameInputValue('')
+    setIsConnected(false)
     console.log('Disconnected from chat room')
   }
 
   return (
-    <div className='flex items-center justify-center bg-blue-50 h-full w-full'>
-      <div className='p-6 bg-white rounded-lg shadow-lg m-12'>
-        <div className='mb-4'>
-          <input
-            className='w-full px-3 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-            type='text'
-            placeholder='이름을 입력하세요'
-            value={nameInputValue}
-            onChange={handleNameInputChange}
-          />
+    <div className='flex items-center justify-center h-[100vh] bg-blue-50 sm:flex p-8'>
+      {!isConnected ? (
+        <div className='p-6 bg-white rounded-lg shadow-lg m-12 transition-opacity duration-500'>
+          <div className='mb-4'>
+            <input
+              className='w-full px-3 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+              type='text'
+              placeholder='이름을 입력하세요'
+              value={nameInputValue}
+              onChange={handleNameInputChange}
+            />
+          </div>
+          <div className='mb-4'>
+            <input
+              className='w-full px-3 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
+              type='text'
+              placeholder='채팅방 ID를 입력하세요'
+              value={roomInputValue}
+              onChange={handleRoomInputChange}
+            />
+          </div>
+          <div className='flex space-x-2'>
+            <button
+              className='px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500'
+              onClick={handleConnectChannel}
+            >
+              접속
+            </button>
+          </div>
         </div>
-        <div className='mb-4'>
-          <input
-            className='w-full px-3 py-2 border border-blue-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-            type='text'
-            placeholder='채팅방 ID를 입력하세요'
-            value={roomInputValue}
-            onChange={handleRoomInputChange}
-          />
-        </div>
-        <div className='flex space-x-2'>
+      ) : (
+        <div className='flex flex-col items-center bg-green-200 rounded-lg'>
+          <StompComponent chatRoomId={chatRoomId} username={username} />
           <button
-            className='px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500'
-            onClick={handlerConnectChannel}
+            className='mb-4 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500'
+            onClick={handleDisconnectChannel}
           >
-            접속
-          </button>
-          <button
-            className='px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-500'
-            onClick={handlerDisconnectChannel}
-          >
-            해체
+            채팅방 나가기
           </button>
         </div>
-      </div>
-      <div>
-        {chatRoomId === '' ? null : <StompComponent chatRoomId={chatRoomId} username={username} />}
-      </div>
+      )}
     </div>
   )
 }
